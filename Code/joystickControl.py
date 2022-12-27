1993e04e5bf2f13a79147db06d63cba7622d7c1a
shrey45 <you@example.com>
2022-12-27 09:23:45 -0500

    startup code

--- Code/joystickControl.py
+++ Code/joystickControl.py
@@ -6,16 +6,17 @@ int motor3pin = ()
 int motor4pin = ()
 int defaultSpeed = ()
 
-LR = ()
-UD = ()
-# make analog (or whatever) values into voltage values or map the values to how the ramge motors operate
+while True: 
+    LR = ()
+    UD = ()
+    # make analog (or whatever) values into voltage values or map the values to how the ramge motors operate
 
-motor1speed = defaultSpeed + (UD - LR)
-motor2speed = defaultSpeed + (UD + LR)
-motor3speed = defaultSpeed + (UD + LR)
-motor4speed = defaultSpeed + (UD - LR)
+    motor1speed = defaultSpeed + (UD - LR)
+    motor2speed = defaultSpeed + (UD + LR)
+    motor3speed = defaultSpeed + (UD + LR)
+    motor4speed = defaultSpeed + (UD - LR)
 
-# make code to send speeds to motors
+    # make code to send speeds to motors
 
 
 
