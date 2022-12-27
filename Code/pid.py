5da7def3bb6450a7bbddb5f2fe9333e9bd32f003
shrey45 <you@example.com>
2022-12-27 07:24:38 -0500

    commit

--- Code/pid.py
+++ Code/pid.py
@@ -1,4 +1,4 @@
-## Code to stablize drone
+# Code to stablize drone
 
 import time
 import board
@@ -11,6 +11,29 @@ i2c = busio.I2C(scl_pin, sda_pin)
 
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
+motor1pin = ()
+motor2pin = ()
+motor3pin = ()
+motor4pin = ()
+const int defaultSpeed = 
+int motor1speed = defaultSpeed
+int motor2speet = defaultSpeed
+int motor3speed = defaultSpeed
+int motor4speed = defaultSpeed
+speedChange = 0
+const float Kp = () # proportional gain
+const float Ki = () # integral gain
+const float Kd = () # derivative gain
+float ingerror
+float lastError
+
+gain = () #how much the sensor changes (dy) with respect to dx (1 notch up) 
+
+
+daccx = (the flat x value)
+daacy = ()
+daacz = ()
+
 error = 0
 
 
@@ -21,12 +44,32 @@ while True:
     z = mpu.acceleration[2]
 
 # calculates error variable
-    
-
+    error = 0
 
 # Converts acceleration values into voltage 0-5
     vx = x/(acc. output range) * 5
     vy = y/() * 5
     vz = z/() * 5
 
-    time.sleep(1)   
+
+
+# PID controller to find speedChange
+
+    joystick1 = () #read analog joystick value
+    targetPitch = map(joystick1, 0, 1023, , )
+    elapsedTime =     #insert function to get time since last loop
+    error = (targetPitch - currentPitch)
+    ingerror = ingerror + error * elapsedTime
+    dxerror = (error - lastError)/elapsedTime
+    speedChange = Kp*error + Ki*ingerror + Kd*dxerror
+    error = lastError
+
+# Changes motor speeds using calculates speedChange variable
+    motor1speed = defaultSpeed + speedChange
+    motor2speet = motor1speed
+    motor3speed = defaultSpeed + speedChange
+    motor4speed = motor3speed
+
+    
+
+
