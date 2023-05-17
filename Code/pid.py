2d3f65d60b506194b569de067f2239f3ec6932ac
Shrey <63983735+shrey45@users.noreply.github.com>
2023-05-17 11:26:19 -0400

    Update pid.py hey it mite work??????

--- Code/pid.py
+++ Code/pid.py
@@ -4,6 +4,8 @@ import board
 import adafruit_mpu6050
 import busio
 import pwmio
+from PID_CPY import PID
+
 
 sda_pin = board.GP10
 scl_pin = board.GP11
@@ -16,47 +18,37 @@ pwm_motor2 = pwmio.PWMOut(board.GP21, duty_cycle=2 ** 15, frequency=50)
 pwm_motor3 = pwmio.PWMOut(board.GP18, duty_cycle=2 ** 15, frequency=50)
 pwm_motor4 = pwmio.PWMOut(board.GP28, duty_cycle=2 ** 15, frequency=50)
 
-motor1speed = 40000
-motor2speed = 40000
-motor3speed = 40000
-motor4speed = 40000
+defaultSpeed = 40000
+
+motor1speed = defaultSpeed
+motor2speed = defaultSpeed
+motor3speed = defaultSpeed
+motor4speed = defaultSpeed
+
+target = 0
+quadrant = []
 
 x = mpu.acceleration[0] / 9.8
 y = mpu.acceleration[1] / 9.8
 z = mpu.acceleration[2] / 9.8
 
-Ki = 0.5
-Kp = 0
-Kd = 0
 
-quadrant = 0
-target = 0
-integralPitch = 0
-integralRoll = 0
-derivativePitch = 0
-derivativeRoll = 0
-lastError = 0
-timeCheck = 0
-integral = 0
-
-def map_range(x, in_min, in_max, out_min, out_max):
-  return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
-
-
+pid = PID(-900, 0, 0, setpoint=target)
+pid.output_limits = (0, 65535)
+#pid.sample_time = 0.01
 
 while True:
+    startTime = time.time()
+    lastTime = startTime
     x = mpu.acceleration[0] / 9.8
     y = mpu.acceleration[1] / 9.8
     z = mpu.acceleration[2] / 9.8
 
     roll = 180 * math.atan(y/math.sqrt(x**2 + z**2))/(math.pi)
-    if roll < 1 or roll > -1 :
-        roll = 0
     pitch = 180 * math.atan(x/(math.sqrt(y**2 + z**2)))/(math.pi)
-    if pitch > -1 and pitch < 1:
-        pitch = 0
-
     hyp = math.sqrt(roll**2 + pitch**2)
+    if hyp < 1.3:
+        hyp = 0
 
     if roll > 0 and pitch > 0:
         quadrant = 1
@@ -66,7 +58,35 @@ while True:
         quadrant = 3
     elif roll < 0 and pitch > 0:
         quadrant = 4
+    #currentTime = time.time()
+    
+    #dt = currentTime - lastTime
+    
+    pidSpeed = pid(hyp)
+    motorSpeed = abs(pidSpeed - defaultSpeed)
+    if quadrant == 1:
+        motor1speed = defaultSpeed + motorSpeed
+        motor3speed = defaultSpeed - motorSpeed
+    if quadrant == 3:
+        motor3speed = defaultSpeed + motorSpeed
+        motor1speed = defaultSpeed - motorSpeed
+    
+    if motor1speed > 65535: 
+        motor1speed = 65535
+    if motor3speed > 65535:
+        motor3speed = 65535
+    
 
+    pwm_motor.duty_cycle = int(motor1speed)
+    pwm_motor3.duty_cycle =int(motor3speed)
+
+
+
+    #print(f" Roll: {roll} & Pitch: {pitch} & Hyp: {hyp} & Speed: {pidSpeed}")
+    print(f"motor1: {motor1speed} & motor 3: {motor3speed} & pidspeed: {pidSpeed} & roll: {roll} & pitch: {pitch} & hyp: {hyp}")
+    #time.sleep(.15)
+
+'''
     currentTime = time.monotonic()
     elapsedTime = currentTime - timeCheck
 
@@ -83,19 +103,5 @@ while True:
     # pwm_motor2.duty_cycle = motor2speed
     # pwm_motor3.duty_cycle = motor3speed
     # pwm_motor4.duty_cycle = motor4speed
-
-
-    timeCheck = currentTime
-    lastError = error
-    print(f" Roll: {roll} & Pitch: {pitch} & Hyp: {hyp} & Speed: {pidSpeed}")
-    time.sleep(.5)
     '''
-    errorPitch = target - pitch
-    errorRoll = target - roll
-
-    integralPitch = integralPitch + elapsedTime * errorPitch
-    integralRoll = integralRoll + elapsedTime * errorRoll
-
-    derivativePitch = (errorPitch - lastErrorPitch)/elapsedTime
-    derivativeRoll = (errorRoll - lastErrorRoll)/elapsedTime
-    '''
+    
